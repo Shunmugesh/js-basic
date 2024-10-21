7985ab6ea3c9bc4d2fc4df3294076c0001f86504
Shunmugesh <110185827+Shunmugesh@users.noreply.github.com>
2024-10-21 08:17:04 +0000

    ops

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,49 @@
+/* let score = "33"
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber) */
+
+
+//  "33" = 33
+//  "33abc"  = NaN
+//  true = 1;  false = 0;
+
+
+/* let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn) */
+
+//  "" = false
+//  "Shunmugesh" = true
+
+
+
+
+
+
+//               OPERATIONS
+
+
+
+// let value = 3
+// let negValue = -value
+
+// console.log(negValue);
+
+
+
+// let str1 = 5
+// let str2 = 10
+
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2")
+// console.log("1" + 2 + "3")
+
+
