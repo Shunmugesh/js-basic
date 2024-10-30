63d7200845330d7dccf6a806a2b01c61fd11de85
Shunmugesh <110185827+Shunmugesh@users.noreply.github.com>
2024-10-30 08:30:52 +0000

    objects

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -1,5 +1,53 @@
+//  const tinderUser = new Object()     =    Singleton Object
+const tinderUser = {}       //    Non Singleton Object
+
+tinderUser.id = "123"
+tinderUser.name = "Shunmugesh"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser)
+
+const regularUser = {
+    email : "some@gaamil.com",
+    fullname : {
+        userfullname : {
+            firstname : "Shunmugesh" ,
+            lastname : "Nadar"
+        }
+    }
+}
+
+//   console.log(regularUser.fullname.userfullname.firstname)
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "c", 4: "d"}
+
+// const obj3 = {obj1, obj2}
+// console.log(obj3)
+
+// const obj3 = Object.assign({}, obj1, obj2)
+// console.log(obj3)
+
+const obj3 = {...obj1, ...obj2}
+// console.log(obj3)
+
+const user= [
+    {
+        id: 1,
+        email: "random@gmail.com"
+    },
+    {
+        id: 2,
+        email: "random2@gmail.com"
+    },
+]
+
+// console.log(user[1].email)
 
 
+console.log(Object.keys(tinderUser))     //return the values in array
+console.log(Object.values(tinderUser))
+console.log(Object.entries(tinderUser))
 
 
 //     node 02_basics/04_objects.js
