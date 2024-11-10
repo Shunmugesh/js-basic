3febe36762ebcbec51fec093d91fba71f0c5d6b8
Shunmugesh <110185827+Shunmugesh@users.noreply.github.com>
2024-11-10 07:47:53 +0000

    loops

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -1,10 +1,10 @@
 const userEmail = []
 
-if (userEmail){
-    console.log("Got user Email")
-}else{
-    cosnole.log("Dont have user email")
-}
+// if (userEmail){
+//     console.log("Got user Email")
+// }else{
+//     cosnole.log("Dont have user email")
+// }
 
 // FALSY VALUE
 
@@ -14,16 +14,31 @@ if (userEmail){
 
 // "0", 'false', " ", [], {}, function(){}, 
 
-if(userEmail.length === 0){
-    console.log("Empty array")
-}
+// if(userEmail.length === 0){
+//     console.log("Empty array")
+// }
 
-const emptyobj = {}
+// const emptyobj = {}
 
-if (Object.keys(emptyobj).length === 0){
-    console.log("Object is  empty")
-}
+// if (Object.keys(emptyobj).length === 0){
+//     console.log("Object is  empty")
+// }
 
 
 //   Nullish Coalescing Operator (??): null undefined
 
+// let val1;
+// val1 = 5 ?? 10
+// val1 = null  ?? 50
+// val1 = undefined ?? 60
+// val1 = null ?? 10 ?? 20
+
+// console.log(val1)
+
+
+// TERNIARY OPERATOR 
+
+// condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("Less than 80") : console.log("more than 80")
---
+++ 05_iterations/dowhile.js
@@ -0,0 +1,27 @@
+//    WHILE
+
+
+index = 0;
+while (index <= 10) {
+    // console.log(`Value of index is ${index}`)
+    index = index + 2;
+}
+
+
+let myArray = ["Thor","Batman","Hulk","Groot","Ironman"]
+let arr = 0
+
+while (arr < myArray.length) {
+    // console.log(`Value is ${myArray[arr]}`);
+    arr = arr + 1;
+}
+
+
+//     dowhile
+
+
+let score = 1;
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
