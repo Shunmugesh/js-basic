9c6e347769fc70cca4649c13f643e20fae72aa54
Shunmugesh <110185827+Shunmugesh@users.noreply.github.com>
2024-11-15 07:09:37 +0000

    dom

---
+++ 05_iterations/reduce.js
@@ -0,0 +1,36 @@
+const myNums = [1, 2, 3]
+
+// const myTotal = myNums.reduce(function(acc, currval) {
+//     console.log(`acc: ${acc} and currval: ${currval}`);
+//     return acc + currval
+// },0)
+
+// const myTotal = myNums.reduce( (acc, currval) => acc+currval, 0 )
+
+// console.log(myTotal)
+
+
+const shoppingCart = [
+    {
+        itemName: "Js course",
+        price: 2999
+    },
+    {
+        itemName: "Java course",
+        price: 5999
+    },
+    {
+        itemName: "Python course",
+        price: 6999
+    }
+]
+
+const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
+console.log(priceToPay)
+
+
+
+
+
+
+// node 05_iterations/reduce.js
